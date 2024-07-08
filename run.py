05ff9efaa8a91c6482c69b08fad6f514e8feb9ed
Ovunda Eche <ovus4real@gmail.com>
2024-07-08 21:07:49 +0000

    Add while loop to request details from uunrs unetails data provare valid

--- run.py
+++ run.py
@@ -16,19 +16,25 @@ data = SHEET.worksheet('data')
 
 details = data.get_all_values()
 
-def get_employee_data():
+def get_employee_detail():
     """
     Get employee details input from the user
     """
-    print("Enter employee details.\n")
-    print("Details should be 6 value: Emp No, Position, Emp Name, Age, Wages, Contract Hours.\n")
-    print("Emp No must be a whole number not decimal, Position should be the rank of the employee, Emp Name should be in alphabets not numbers, Age should be whole number not decimal, Wages should be numbers, Contract Hours can be whole or decimal number and each details should be separated by commas.\n")
-    print("Example: 001,Sales Manager,Robert Albert,20,30000,42\n")
+    while True:
+        print("Enter employee details.\n")
+        print("Details should be 6 value: Emp No, Position, Emp Name, Age, Wages, Contract Hours.\n")
+        print("Emp No must be a whole number not decimal, Position should be the rank of the employee, Emp Name should be in alphabets not numbers, Age should be whole number not decimal, Wages should be numbers, Contract Hours can be whole or decimal number and each details should be separated by commas.\n")
+        print("Example: 001,Sales Manager,Robert Albert,20,30000,42\n")
 
-    detail_str = input("Enter employee details: ")
+        detail_str = input("Enter employee details: ")
 
-    employee_detail = detail_str.split(",")
-    validate_data(employee_detail)
+        employee_detail = detail_str.split(",")
+        
+        if validate_data(employee_detail):
+            print("Employee details are valid!")
+            break
+
+    return employee_detail
 
 def validate_data(details):
     """
@@ -37,36 +43,46 @@ def validate_data(details):
     or if there aren't the same 6 details.
     """
     try:
+        if len(details) != 5:
+            raise ValueError(
+                f"Invalid list of employee details entered,6 details are required and you entered {len(details)} details"
+            )
+
         if not details[0].isdigit():
             raise ValueError(
                 f"Invalid data type for {details[0]}. Expected EmpNo in integer"
             )
+
         if not is_alpha_or_space(details[1]):
             raise ValueError(
-                f"Invalid data type for {is_alpha_or_space(details[1])}. Expected employee position in alphabet"
+                f"Invalid data type for {(details[1])}. Expected employee position in alphabet"
             )
+
         if not is_alpha_or_space(details[2]):
             raise ValueError(
-                f"Invalid data type for {is_alpha_or_space(details[2])}. Expected employee name in alphabet"
+                f"Invalid data type for {(details[2])}. Expected employee name in alphabet"
             )
+
         if not details[3].isdigit():
             raise ValueError(
                 f"Invalid data type for {details[3]}. Expected employee age in integer"
             )
+
         if not details[4].isdigit():
             raise ValueError(
                 f"Invalid data type for {details[4]}. Expected employee wages in integer"
             )
+
         if not details[5].isdigit():
             raise ValueError(
                 f"Invalid data type for {details[5]}. Expected employee contract hours in digit"
             )
-        if len(details) != 6:
-            raise ValueError(
-                f"Invalid list of employee details entered, 6 details are required and you entered {len(details)} details"
-            )
+
     except ValueError as e:
         print(f"Validation error: {e}, please try again.\n")
+        return False
+
+    return True
 
 def is_alpha_or_space(string):
     for char in string:
@@ -74,7 +90,7 @@ def is_alpha_or_space(string):
             return False
         return True
 
-get_employee_data()
+detail = get_employee_detail()
 
 
 
