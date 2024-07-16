a2b82c76e8ad09414f4409ecdf2593b719d68759
Ovunda Eche <ovus4real@gmail.com>
2024-07-16 13:32:22 +0000

    Add four colors to the syntax

--- run.py
+++ run.py
@@ -25,14 +25,14 @@ def get_employee_detail():
     by commas. The loop will repeatedly request employee details, until details provided are valid.
     """
     while True:
-        print("{Fore.YELLOW}Example: 001,Sales Manager,Robert Albert,20,30000,42\n")
+        print(Fore.RED + "Example: 001,Sales Manager,Robert Albert,20,30000,42\n")
 
-        detail_str = input("Enter employee details:\n")
+        detail_str = input(Fore.WHITE + "Enter employee details:\n")
 
         employee_detail = detail_str.split(",")
         
         if validate_data(employee_detail):
-            print("Employee details are valid!")
+            print(Fore.YELLOW + "Employee details are valid!\n")
             break
 
     return employee_detail
@@ -46,44 +46,44 @@ def validate_data(details):
     try:
         if len(details) != 6:
             raise ValueError(
-                f"Invalid list of employee details entered,6 details are required and you entered {len(details)} details"
+                Fore.RED + f"Invalid list of employee details entered,6 details are required and you entered {len(details)} details"
             )
 
         if not details[0].isdigit():
             raise ValueError(
-                f"Invalid data type for {details[0]}. Expected EmpNo in digit"
+                Fore.RED + f"Invalid data type for {details[0]}. Expected EmpNo in digit"
             )
 
         if not is_alpha_or_space(details[1]):
             raise ValueError(
-                f"Invalid data type for {(details[1])}. Expected employee position in alphabet"
+                Fore.RED + f"Invalid data type for {(details[1])}. Expected employee position in alphabet"
             )
 
         if not is_alpha_or_space(details[2]):
             raise ValueError(
-                f"Invalid data type for {(details[2])}. Expected employee name in alphabet"
+                Fore.RED + f"Invalid data type for {(details[2])}. Expected employee name in alphabet"
             )
 
         if not details[3].isdigit():
             raise ValueError(
-                f"Invalid data type for {details[3]}. Expected employee age in digit"
+                Fore.RED + f"Invalid data type for {details[3]}. Expected employee age in digit"
             )
 
         if not details[4].isdigit():
             raise ValueError(
-                f"Invalid data type for {details[4]}. Expected employee annual wages in digit"
+                Fore.RED + f"Invalid data type for {details[4]}. Expected employee annual wages in digit"
             )
 
         if not details[5].isdigit():
             raise ValueError(
-                f"Invalid data type for {details[5]}. Expected employee contract hours in digit"
+                Fore.RED + f"Invalid data type for {details[5]}. Expected employee contract hours in digit"
             )
 
     except IndexError:
         raise ValueError("List doesn't have enough items")
 
     except ValueError as e:
-        print(f"Validation error: {e}, please try again.\n")
+        print(Fore.WHITE + f"Validation error: {e}, please try again.\n")
         return False
 
     return True
@@ -99,17 +99,18 @@ def update_employee_data_worksheet(detail):
     print("Updating worksheet with employee details entered...\n")
     detail_worksheet = SHEET.worksheet("data")
     detail_worksheet.append_row(detail)
-    print("You have successfully updated employee data in worksheet.\n")
+    print(Fore.YELLOW + "You have successfully updated employee data in worksheet.\n")
 
 def main_employee_detail():
     """Run all functions in program"""
     detail = get_employee_detail()
     update_employee_data_worksheet(detail)
 
-print(f"""
+print(Fore.WHITE + f"""
 Welcome to Oyistra Staff Data Storage Board\n
 Enter employee details.\n
 Details should be 6 value: Emp No, Position, Emp Name, Age, Wages, Contract Hours.\n
 Emp No must be a whole number not decimal, Position should be the rank of the employee in alphabets, Name should be in alphabets not numbers, Age, Wages and Contract Hours should be whole numbers not decimal and each details should be separated by commas.\n
 """)
+
 main_employee_detail()
